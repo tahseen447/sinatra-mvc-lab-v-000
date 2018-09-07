701c3065ea7a62b5da9ba3d7eaba8ae5db821985
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-07 21:26:58 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,11 @@ class PigLatinizer
     new_word_array=[]
     words = text.split(' ')
     words.each do |word|
-      new_word = ''
+      length= word.length
+      first_letter = word[0]
+      new_word = word.slice(1..length-1)
+      new_word += first_letter
+      new_word += "ay"
 
     end
   end
