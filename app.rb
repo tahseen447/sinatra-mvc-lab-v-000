adad83c78f48536b881942da122695dcc65fd116
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-07 22:21:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
+  post '/piglatinize' do
     pl = PigLatinizer.new
     @piglatin = pl.piglatinize(params[:user_phrase])
     erb :piglatinize
