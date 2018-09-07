f959b61d4f7ba792fc4c67e4539e25368bfb84a8
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-07 22:11:33 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,39 @@
 class PigLatinizer
 
-
-  def initialize()
+  def piglatinize(input_str)
+    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    puts x
+    x
   end
 
-  def piglatinize(text)
-    new_word_array=[]
-    words = text.split(' ')
-    words.each do |word|
-      length= word.length
-      first_letter = word[0]
-      new_word =''
-      new_word = word.slice(1..length-1)
-      new_word += first_letter
-      new_word += "ay"
-      new_word_array << new_word
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
+    else
+      word = word.slice(1..-1) + word.slice(0)
     end
-    new_word_array.join(' ')
+    word << "ay"
   end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+
+
+
 end
